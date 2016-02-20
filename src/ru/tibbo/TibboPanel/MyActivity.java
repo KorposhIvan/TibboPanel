9ead37605227f6b1d21848d78f5397bcd3487609
Ivan <ikorposh@gmail.com>
2016-02-21 00:40:27 +0300

    Реализовано: обмен данными между сервисом и активити. Протокол приема сообщений и обновления GUI. Осталось: фанкойл (дизайн+упр), вывод системных сообщений (дизайн+упр), протокол отправка, меню (дизайн+упр.). Текст отказа от ответственности. Лицензионное соглашение. Продумать алгоритм уведомлений. Рекконекта при разрыве.

--- src/ru/tibbo/TibboPanel/MyActivity.java
+++ src/ru/tibbo/TibboPanel/MyActivity.java
@@ -18,14 +18,17 @@ public class MyActivity extends Activity {
 
     public final static String BROADCAST_ACTION = "ru.tibbo.TibboPanel";
     public final static String RMESSAGE = "RECEIVED";
-    TextView tvRT, tvST, tvSetTemp;
+    TextView tvRT, tvST, tvSetTemp, tvStatus;
     ImageButton btnHFon,btnHFoff,btnModeMan,btnModeAuto;
     GridLayout modegridm, modegrida;
     BroadcastReceiver br;
     String goalTemp = "+%1$d C";
+    String strTemp = "%1$s C";
     int CurrSTemp = 22;
     int MaxSTemp = 27;
     int MinSTemp = 17;
+    String[] answer_controller = new String[16];
+    String[] command_controller = new String[23];
 
     /** Called when the activity is first created. */
     @Override
@@ -35,6 +38,7 @@ public class MyActivity extends Activity {
 
         modegridm = (GridLayout) findViewById(R.id.modegridm);
         modegrida = (GridLayout) findViewById(R.id.modegrida);
+        tvStatus = (TextView) findViewById(R.id.tvStatus);
         tvRT = (TextView) findViewById(R.id.tvRoomTemp);
         tvST = (TextView) findViewById(R.id.tvStreetTemp);
         tvSetTemp = (TextView) findViewById(R.id.tvSetTemp);
@@ -42,18 +46,59 @@ public class MyActivity extends Activity {
         btnHFoff = (ImageButton) findViewById(R.id.imbtnHFoff);
         btnModeMan = (ImageButton) findViewById(R.id.imbtnMdman);
         btnModeAuto = (ImageButton) findViewById(R.id.imbtnMdauto);
-        btnHFoff.setEnabled(false);
-        btnModeAuto.setEnabled(false);
-        tvRT.setText("--");
-        tvST.setText("--");
-        tvSetTemp.setText(String.format(goalTemp,CurrSTemp));
+
+        //Команды для контроллера
+        command_controller[0] = "00"; //Получить все данные
+        command_controller[1] = "01"; //Получить температуру на улице
+        command_controller[2] = "02"; //Получить температуру в комнате
+        command_controller[3] = "03"; //Получить влажность
+        command_controller[4] = "04"; //Получить состояние теплого пола (вкл.выкл)
+        command_controller[5] = "05"; //Получить состояние фанкойла (выкл.1.2.3.)
+        command_controller[6] = "06"; //Получить состояние протечки
+        command_controller[7] = "07"; //Включить теплый пол
+        command_controller[8] = "08"; //Выключить теплый пол
+        command_controller[9] = "09"; //Включить фанкойл 1
+        command_controller[10] = "10"; //Включить фанкойл 2
+        command_controller[11] = "11"; //Включить фанкойл 3
+        command_controller[12] = "12"; //Выключить фанкойл
+        command_controller[13] = "13"; //Затенение вверх (в данных номер окна)
+        command_controller[14] = "14"; //Затенение вниз (в данных номер окна)
+        command_controller[15] = "15"; //Приточка включить
+        command_controller[16] = "16"; //Приточка выключить
+        command_controller[17] = "17"; //Получить состояние заслонки
+        command_controller[18] = "18"; //Получить режим (зима/лето)
+        command_controller[19] = "19"; //Получить режим (авто/ручной)
+        command_controller[20] = "20"; //Задать режим (авто/ручной)
+        command_controller[21] = "21"; //Задать температуру
+        command_controller[22] = "22"; //Получить заданную температуру
+
+        //Инициализация массива ответа. Потом заменить на значения из preferences
+        answer_controller[0] = "FF"; //Символ начала пакета
+        answer_controller[1] = "001"; //ID комнаты
+        answer_controller[2] = "00"; //Команда. 00 - получить все
+        answer_controller[3] = "--"; //Температура улицы
+        answer_controller[4] = "--"; //Температура комнаты
+        answer_controller[5] = "22"; //Заданная температура
+        answer_controller[6] = "0.5"; //Дельта температур
+        answer_controller[7] = "0"; //Режим автомата
+        answer_controller[8] = "1"; //Режим зима лето
+        answer_controller[9] = "--"; //Влажность
+        answer_controller[10] = "0"; //Состояние теплого пола
+        answer_controller[11] = "0"; //Состояние фанкойла
+        answer_controller[12] = "0"; //Состояние приточки
+        answer_controller[13] = " "; //Сообщение информации
+        answer_controller[14] = " "; //Сообщение системное
+        answer_controller[15] = " "; //Сообщение фатальной ошибки
+
+        //Первичная инициализация экрана
+        GuiUpdate(answer_controller);
+
         // создаем BroadcastReceiver
         br = new BroadcastReceiver() {
             // действия при получении сообщений
             public void onReceive(Context context, Intent intent) {
-                String[] result = Parse(intent.getStringExtra(RMESSAGE));
-                tvST.setText(result[0]);
-                tvRT.setText(result[1]);
+                answer_controller = Parse(intent.getStringExtra(RMESSAGE));
+                GuiUpdate(answer_controller);
             }
         };
 
@@ -105,7 +150,7 @@ public class MyActivity extends Activity {
     }
 
     public String[] Parse(String message) {
-        String[] temp;
+        /*String[] temp;
         String[] parseresult = new String[2];
         temp = message.split(";");
         if (temp.length == 1) {
@@ -116,11 +161,60 @@ public class MyActivity extends Activity {
             parseresult[0] = temp[0];
             parseresult[1] = temp[1];
         }
-        return parseresult;
+        return parseresult;*/
+        String[] parseresult = new String[16];
+        parseresult = message.split(";");
+        if (parseresult.length == 1) {return answer_controller;} else {return parseresult;}
+    }
+
+    public void GuiUpdate(String[] param) {
+        //Обновление экрана
+        String stat;
+        tvST.setText(String.format(strTemp,param[3]));
+        tvRT.setText(String.format(strTemp,param[4]));
+        stat = "Статус: ТП ";
+        if (param[10].equals("0")) {stat += "выкл; ";} else {stat += "вкл; ";}
+        stat += "Конд ";
+        if (param[11].equals("0")) {stat += "выкл; ";} else {stat += "вкл; ";}
+        stat += "Пр ";
+        if (param[12].equals("0")) {stat += "выкл; " ;} else {stat += "вкл; ";}
+        tvStatus.setText(stat);
+        CurrSTemp = Integer.parseInt(param[5]);
+        //Если режим автомата (0) или режим ручной (1)
+        if (param[7].equals("0")) {
+            btnModeAuto.setBackgroundResource(R.drawable.mdaten);
+            btnModeMan.setBackgroundResource(R.drawable.mdmandis);
+            btnModeAuto.setEnabled(false);
+            btnModeMan.setEnabled(true);
+            modegrida.setVisibility(modegridm.VISIBLE);
+            modegridm.setVisibility(modegrida.GONE);
+            tvSetTemp.setText(String.format(goalTemp,CurrSTemp));
+        }
+        else {
+            btnModeMan.setBackgroundResource(R.drawable.mdmanen);
+            btnModeAuto.setBackgroundResource(R.drawable.mdatdis);
+            btnModeMan.setEnabled(false);
+            btnModeAuto.setEnabled(true);
+            modegridm.setVisibility(modegridm.VISIBLE);
+            modegrida.setVisibility(modegridm.GONE);
+            if (param[10].equals("0")) {
+                btnHFon.setBackgroundResource(R.drawable.tpondis);
+                btnHFoff.setBackgroundResource(R.drawable.tpoffen);
+                btnHFon.setEnabled(true);
+                btnHFoff.setEnabled(false);
+            }
+            else {
+                btnHFon.setBackgroundResource(R.drawable.tponen);
+                btnHFoff.setBackgroundResource(R.drawable.tpoffdis);
+                btnHFon.setEnabled(false);
+                btnHFoff.setEnabled(true);
+            }
+        }
     }
 
     public void onBtnHFon (View v) {
-        startService(new Intent(this,TibboService.class).putExtra("Command","TPon"));
+        startService(new Intent(this,TibboService.class).putExtra("Command",answer_controller[0]));
+        //startService(new Intent(this,TibboService.class).putExtra("Command","TPon"));
         btnHFon.setBackgroundResource(R.drawable.tponen);
         btnHFoff.setBackgroundResource(R.drawable.tpoffdis);
         btnHFon.setEnabled(false);
@@ -128,7 +222,7 @@ public class MyActivity extends Activity {
     }
 
     public void onBtnHFoff (View v) {
-        startService(new Intent(this,TibboService.class).putExtra("Command","TPoff"));
+        startService(new Intent(this,TibboService.class).putExtra("Command","Привет"));
         btnHFon.setBackgroundResource(R.drawable.tpondis);
         btnHFoff.setBackgroundResource(R.drawable.tpoffen);
         btnHFon.setEnabled(true);
